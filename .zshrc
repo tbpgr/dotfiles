61b9ac0ac09356db4169d54dd7f1befe00f3e2db
tbpgr <tbpgr@tbpgr.jp>
2015-12-17 23:36:05 +0900

    :up: modify: update

--- .zshrc
+++ .zshrc
@@ -10,12 +10,16 @@ if [[ -s "${ZDOTDIR:-$HOME}/.zprezto/init.zsh" ]]; then
   source "${ZDOTDIR:-$HOME}/.zprezto/init.zsh"
 fi
 
-# execute configs
-for config_file ($HOME/.yadr/zsh/*.zsh) source $config_file
-
 alias cc='clear'
 alias pcd='cd $(find . -maxdepth 1 -type d | peco)'
-alias baskup_todos='ruby /Users/tbpgr/todos/bakup_daily.rb'
 function git(){hub "$@"}
 bindkey '^@' gcd
+
+if [ -f /usr/local/bin/virtualenvwrapper.sh ]; then
+    export WORKON_HOME=$HOME/.virtualenvs
+        source /usr/local/bin/virtualenvwrapper.sh
+fi
+
+export PATH="$HOME/.anyenv/bin:$PATH"
+eval "$(anyenv init -)"
 
--- setup.sh
+++ setup.sh
@@ -1,18 +1,50 @@
 #!/bin/sh
 
 cd $HOME
+
+echo "start setup"
+
 # install Homebrew
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-ln -s dotfiles/Brewfile
-# install brew bundle
-brew tap Homebrew/bundle
-# change default shell to zsh
-chsh -s /usr/local/bin/zsh
+if ! [ -e /usr/local/bin/brew ]; then
+  echo "\tinstall Homebrew"
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+  echo "\tset slink Brewfile"
+  ln -s dotfiles/Brewfile
+
+  echo "\ttap Homebrew/bundle"
+  brew tap Homebrew/bundle
+  # change default shell to zsh
+  chsh -s /usr/local/bin/zsh
+fi
+
+echo "\tinstall brew packages"
 brew bundle
 
-git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+if ! [ -e ${ZDOTDIR:-$HOME}/.zprezto ]; then
+  git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+  ln -s dotfiles/.zpreztorc
+  ln -s dotfiles/.zsh.after
+  ln -s dotfiles/.zshrc
+fi
 
 # ** link dotfiles **
-ln -s dotfiles/.zpreztorc
-ln -s dotfiles/.zsh.after
-ln -s dotfiles/.zshrc
+for file in .zpreztorc .zshrc
+do
+  if ! [ -L "${ZDOTDIR:-$HOME}/$file" ]; then
+    echo "\tlink dotfiles"
+    ln -s "dotfiles/${file}"
+  fi
+done
+
+
+if ! [ -e `echo $PATH|grep anyenv` ]; then
+  echo "\tclone anyenv"
+  git clone https://github.com/riywo/anyenv ~/.anyenv
+
+  echo "\tanyenv initial settings"
+  echo 'export PATH="$HOME/.anyenv/bin:$PATH"' >> ~/dotfiles/.zshrc
+  echo 'eval "$(anyenv init -)"' >> ~/dotfiles/.zshrc
+fi
+
+echo "finish setup"
